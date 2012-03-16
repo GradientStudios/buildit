819523a2e1db2aad91adebcbecef1ac086dd24ed
Michael "Z" Goddard <mzgoddard@gmail.com>
2012-03-16 18:51:16 -0400

    jobqueue ticks every 50ms and uses late added jobs

--- test/tests/jobqueue.js
+++ test/tests/jobqueue.js
@@ -1,7 +1,7 @@
 var jobqueue = require('../../lib/jobqueue');
 
 exports.testJobQueue = function(test) {
-  test.expect(3);
+  test.expect(4);
 
   var jq = jobqueue.jobQueue();
   jq.add(function(next) {
@@ -10,12 +10,17 @@ exports.testJobQueue = function(test) {
   });
   jq.add(function(next) {
     test.ok(true);
+
+    setTimeout(function() {
+      test.ok(true);
+      jq.add(function(next) {
+        test.ok(true);
+        next();
+        test.done();
+      });
+    }, 100);
+
     next();
   });
-  jq.add(function(next) {
-    test.ok(true);
-    next();
-    test.done();
-  })
   jq.start();
 };
